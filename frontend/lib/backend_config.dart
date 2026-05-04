622f3f2b9b43652b46000d529867c4cf626d40b2
Mashfi <mashfimahin2003@gmail.com>
2026-05-04 19:30:34 +0600

    prepared to deploy

--- frontend/lib/backend_config.dart
+++ frontend/lib/backend_config.dart
@@ -1,4 +1,8 @@
 import 'src/backend_config_io.dart'
     if (dart.library.html) 'src/backend_config_web.dart';
 
-String get backendUrl => backendUrlImpl();
+// Set this to true for deployment and update prodUrl with your Render/Backend URL
+const bool isProduction = false;
+const String prodUrl = 'https://cholo-backend.onrender.com';
+
+String get backendUrl => isProduction ? prodUrl : backendUrlImpl();
